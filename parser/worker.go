2ddb5650cfacb26ee97a60445c1016edca8055fc
user <user@MacBook-Pro-user.local>
2022-05-07 16:38:45 +0300

    ---

--- parser/worker.go
+++ parser/worker.go
@@ -3,6 +3,7 @@ package parser
 import (
 	"encoding/json"
 	"fmt"
+	"runtime/debug"
 
 	"github.com/nuclearblock/archgregator/logging"
 
@@ -116,28 +117,7 @@ func (w Worker) Process(height int64) error {
 		return fmt.Errorf("failed to get transactions for block: %s", err)
 	}
 
-	vals, err := w.node.Validators(height)
-	if err != nil {
-		return fmt.Errorf("failed to get validators for block: %s", err)
-	}
-
-	return w.ExportBlock(block, events, txs, vals)
-}
-
-// ProcessTransactions fetches transactions for a given height and stores them into the database.
-// It returns an error if the export process fails.
-func (w Worker) ProcessTransactions(height int64) error {
-	block, err := w.node.Block(height)
-	if err != nil {
-		return fmt.Errorf("failed to get block from node: %s", err)
-	}
-
-	txs, err := w.node.Txs(block)
-	if err != nil {
-		return fmt.Errorf("failed to get transactions for block: %s", err)
-	}
-
-	return w.ExportTxs(txs)
+	return w.ExportBlock(block, events, txs)
 }
 
 // HandleGenesis accepts a GenesisDoc and calls all the registered genesis handlers
@@ -155,65 +135,22 @@ func (w Worker) HandleGenesis(genesisDoc *tmtypes.GenesisDoc, appState map[strin
 	return nil
 }
 
-// SaveValidators persists a list of Tendermint validators with an address and a
-// consensus public key. An error is returned if the public key cannot be Bech32
-// encoded or if the DB write fails.
-func (w Worker) SaveValidators(vals []*tmtypes.Validator) error {
-	var validators = make([]*types.Validator, len(vals))
-	for index, val := range vals {
-		consAddr := sdk.ConsAddress(val.Address).String()
-
-		consPubKey, err := types.ConvertValidatorPubKeyToBech32String(val.PubKey)
-		if err != nil {
-			return fmt.Errorf("failed to convert validator public key for validators %s: %s", consAddr, err)
-		}
-
-		validators[index] = types.NewValidator(consAddr, consPubKey)
-	}
-
-	err := w.db.SaveValidators(validators)
-	if err != nil {
-		return fmt.Errorf("error while saving validators: %s", err)
-	}
-
-	return nil
-}
-
 // ExportBlock accepts a finalized block and a corresponding set of transactions
 // and persists them to the database along with attributable metadata. An error
 // is returned if the write fails.
 func (w Worker) ExportBlock(
-	b *tmctypes.ResultBlock, r *tmctypes.ResultBlockResults, txs []*types.Tx, vals *tmctypes.ResultValidators,
-) error {
-	// Save all validators
-	err := w.SaveValidators(vals.Validators)
-	if err != nil {
-		return err
-	}
-
-	// Make sure the proposer exists
-	proposerAddr := sdk.ConsAddress(b.Block.ProposerAddress)
-	val := findValidatorByAddr(proposerAddr.String(), vals)
-	if val == nil {
-		return fmt.Errorf("failed to find validator by proposer address %s: %s", proposerAddr.String(), err)
-	}
+	b *tmctypes.ResultBlock, r *tmctypes.ResultBlockResults, txs []*types.Tx) error {
 
 	// Save the block
-	err = w.db.SaveBlock(types.NewBlockFromTmBlock(b, sumGasTxs(txs)))
+	err := w.db.SaveBlock(types.NewBlockFromTmBlock(b, sumGasTxs(txs)))
 	if err != nil {
 		return fmt.Errorf("failed to persist block: %s", err)
-	}
-
-	// Save the commits
-	err = w.ExportCommit(b.Block.LastCommit, vals)
-	if err != nil {
-		return err
 	}
 
 	// Call the block handlers
 	for _, module := range w.modules {
 		if blockModule, ok := module.(modules.BlockModule); ok {
-			err = blockModule.HandleBlock(b, r, txs, vals)
+			err = blockModule.HandleBlock(b, r, txs)
 			if err != nil {
 				w.logger.BlockError(module, b, err)
 			}
@@ -221,83 +158,12 @@ func (w Worker) ExportBlock(
 	}
 
 	// Export the transactions
-	return w.ExportTxs(txs)
+	return w.ExportEvents(r)
 }
 
-// ExportCommit accepts a block commitment and a corresponding set of
-// validators for the commitment and persists them to the database. An error is
-// returned if any write fails or if there is any missing aggregated data.
-func (w Worker) ExportCommit(commit *tmtypes.Commit, vals *tmctypes.ResultValidators) error {
-	var signatures []*types.CommitSig
-	for _, commitSig := range commit.Signatures {
-		// Avoid empty commits
-		if commitSig.Signature == nil {
-			continue
-		}
-
-		valAddr := sdk.ConsAddress(commitSig.ValidatorAddress)
-		val := findValidatorByAddr(valAddr.String(), vals)
-		if val == nil {
-			return fmt.Errorf("failed to find validator by commit validator address %s", valAddr.String())
-		}
-
-		signatures = append(signatures, types.NewCommitSig(
-			types.ConvertValidatorAddressToBech32String(commitSig.ValidatorAddress),
-			val.VotingPower,
-			val.ProposerPriority,
-			commit.Height,
-			commitSig.Timestamp,
-		))
-	}
-
-	err := w.db.SaveCommitSignatures(signatures)
-	if err != nil {
-		return fmt.Errorf("error while saving commit signatures: %s", err)
-	}
-
-	return nil
-}
-
-// ExportTxs accepts a slice of transactions and persists then inside the database.
-// An error is returned if the write fails.
-func (w Worker) ExportTxs(txs []*types.Tx) error {
-	// Handle all the transactions inside the block
-	for _, tx := range txs {
-		// Save the transaction itself
-		err := w.db.SaveTx(tx)
-		if err != nil {
-			return fmt.Errorf("failed to handle transaction with hash %s: %s", tx.TxHash, err)
-		}
-
-		// Call the tx handlers
-		for _, module := range w.modules {
-			if transactionModule, ok := module.(modules.TransactionModule); ok {
-				err = transactionModule.HandleTx(tx)
-				if err != nil {
-					w.logger.TxError(module, tx, err)
-				}
-			}
-		}
-
-		// Handle all the messages contained inside the transaction
-		for i, msg := range tx.Body.Messages {
-			var stdMsg sdk.Msg
-			err = w.codec.UnpackAny(msg, &stdMsg)
-			if err != nil {
-				return fmt.Errorf("error while unpacking message: %s", err)
-			}
-
-			// Call the handlers
-			for _, module := range w.modules {
-				if messageModule, ok := module.(modules.MessageModule); ok {
-					err = messageModule.HandleMsg(i, stdMsg, tx)
-					if err != nil {
-						w.logger.MsgError(module, tx, stdMsg, err)
-					}
-				}
-			}
-		}
-	}
+// ExportEvents accepts block events and process it. 
+// An error is returned if fails.
+func (w Worker) ExportEvents(r *tmctypes.ResultBlockResults) error {
 
 	return nil
 }
