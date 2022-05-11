5c157b86b4fbd670f5feda4d07a49fed06953cfb
user <user@MacBook-Pro-user.local>
2022-05-11 08:03:41 +0300

    ---

--- parser/message_handler.go
+++ parser/message_handler.go
@@ -1,38 +1,38 @@
 package parser
 
-import (
-	"github.com/cosmos/cosmos-sdk/codec"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/gogo/protobuf/proto"
+// import (
+// 	"github.com/cosmos/cosmos-sdk/codec"
+// 	sdk "github.com/cosmos/cosmos-sdk/types"
+// 	"github.com/gogo/protobuf/proto"
 
-	"github.com/nuclearblock/archgregator/database"
-	"github.com/nuclearblock/archgregator/types"
-)
+// 	"github.com/nuclearblock/archgregator/database"
+// 	"github.com/nuclearblock/archgregator/types"
+// )
 
-// HandleMsg represents a message handler that stores the given message inside the proper database table
-func HandleMsg(
-	index int, msg sdk.Msg, tx *types.Tx,
-	parseAddresses MessageAddressesParser, cdc codec.Codec, db database.Database,
-) error {
+// // HandleMsg represents a message handler that stores the given message inside the proper database table
+// func HandleMsg(
+// 	index int, msg sdk.Msg, tx *types.Tx,
+// 	parseAddresses MessageAddressesParser, cdc codec.Codec, db database.Database,
+// ) error {
 
-	// Get the involved addresses
-	addresses, err := parseAddresses(cdc, msg)
-	if err != nil {
-		return err
-	}
+// 	// Get the involved addresses
+// 	addresses, err := parseAddresses(cdc, msg)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	// Marshal the value properly
-	bz, err := cdc.MarshalJSON(msg)
-	if err != nil {
-		return err
-	}
+// 	// Marshal the value properly
+// 	bz, err := cdc.MarshalJSON(msg)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	return db.SaveMessage(types.NewMessage(
-		tx.TxHash,
-		index,
-		proto.MessageName(msg),
-		string(bz),
-		addresses,
-		tx.Height,
-	))
-}
+// 	return db.SaveMessage(types.NewMessage(
+// 		tx.TxHash,
+// 		index,
+// 		proto.MessageName(msg),
+// 		string(bz),
+// 		addresses,
+// 		tx.Height,
+// 	))
+// }
