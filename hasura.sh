ba87069148c31381f3f7a614142ba37ad774a945
user <user@MacBook-Pro-user.local>
2022-05-18 03:49:55 +0300

    ---

--- hasura.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-docker run -d -p 8080:8080 \
-       -e HASURA_GRAPHQL_DATABASE_URL=postgres://archway:password@localhost:5432/archway \
-       -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
-       -e HASURA_GRAPHQL_DEV_MODE=true \
-       hasura/graphql-engine:v2.6.2
